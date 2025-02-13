9a7d782be137c7ea73c8f8f92f8870f17a1e7f24
thatone-dumb-blob <156300153+thatone-dumb-blob@users.noreply.github.com>
2025-02-13 16:52:01 +0700

    Actual Player movement

--- Assets/Playermovement.cs
+++ Assets/Playermovement.cs
@@ -1,21 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
         public float moveSpeed = 5f; // Movement speed of the player
-        private Vector2 movement; // Vector to store movement input
-        
-    }
 
     // Update is called once per frame
     void Update()
-    {
-        float movement.x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
-        float movement.y = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+    {   
+        float movex = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        float movey = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+    transform.Translate(new Vector3(movex,movey,0));
     }
 }
