63d87448b44e86ee452277a1cfbd9b76dc05e53c
thatone-dumb-blob <156300153+thatone-dumb-blob@users.noreply.github.com>
2025-02-20 16:46:34 +0700

    floor and sprint

--- Assets/Playermovement.cs
+++ Assets/Playermovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-        public float moveSpeed = 10f; // Movement speed of the player
+        public float moveSpeed = 7f; // Movement speed of the player
 
     // Update is called once per frame
     void Update()
@@ -11,5 +11,13 @@ public class Playermovement : MonoBehaviour
         float movex = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
         float movey = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
     transform.Translate(new Vector3(movex,movey,0));
+        if (Input.GetKeyDown(KeyCode.LeftShift)) 
+        {
+            moveSpeed = 30f;
+        }
+        else
+        {
+            moveSpeed = 7f;
+        }
     }
 }
